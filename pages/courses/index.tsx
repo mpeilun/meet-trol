5593df6dbcdfa46258930286ae5808860635b5a9
mpeilun <peilun2016@gmail.com>
2023-01-12 23:28:58 +0800

    update

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -2,35 +2,46 @@ import { useRouter } from 'next/router'
 
 import CourseList from '../../components/courses/course-list'
 import { getAllCourses } from '../../lib/dummy-data'
-import { AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Avatar, Button, Tooltip, MenuItem, TextField } from '@mui/material'
+import { AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Avatar, Button, Tooltip, MenuItem, TextField, Paper } from '@mui/material'
 import Grid from '@mui/material/Unstable_Grid2' // Grid version 2
 import { grey } from '@mui/material/colors'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 
 function AllCoursesPage() {
   return (
-    <>
-      <Grid container flexDirection="row-reverse" justifyContent="space-between" spacing={2} width={'80%'} height={'100%'} maxWidth={'xl'} m={'5rem auto'} p={'2rem'} sx={{ backgroundColor: '#fff' }}>
+    <Paper sx={{ width: { md: '80%', xs: ' 100%' }, height: '100%', maxWidth: 'xl', m: { md: '5rem auto', xs: '0' }, p: '2rem', backgroundColor: '#FFF' }}>
+      <Grid container flexDirection="row-reverse" justifyContent="space-between" spacing={2}>
         <Grid md={12} xs={12} display="flex" justifyContent="center" alignContent="center" m={2}>
-          <TextField sx={{ width: '50%', minWidth: '100px' }}></TextField>
+          <TextField placeholder="加入課程" sx={{ width: '50%', minWidth: '100px' }}></TextField>
           {/*TODO 考慮使用ICON Button */}
-          <Button variant="contained" sx={{ ml: '5%' }}>
-            <Typography>加入課程</Typography>
+          <Button variant="contained" sx={{ ml: '1rem', height: '95%' }}>
+            <ArrowForwardIcon />
           </Button>
         </Grid>
         <Grid md={12} xs={12}>
           <Box display="flex" flexDirection="column" m={2}>
-            <Typography variant="h5">我的課程</Typography>
-            {(function () {
-              let elements = []
-              for (let i = 0; i < 10; i++) {
-                elements.push(<Box key={i} mt={1} width={'100%'} height={'8rem'} bgcolor={'#000'}></Box>)
-              }
-              return elements
-            })()}
+            <Typography variant="h5" m={'0 auto'}>
+              我的課程
+            </Typography>
+            <Grid container spacing={1} mt={1} justifyContent={{ md: 'center', xs: 'center' }}>
+              {(function () {
+                let elements = []
+                for (let i = 0; i < 10; i++) {
+                  elements.push(
+                    <Grid md={4} xs={12} key={i}>
+                      <Box m={'auto'} width={'100%'} maxWidth="200px" height={'200px'}>
+                        <Typography bgcolor={grey[400]}>課程名稱</Typography>
+                      </Box>
+                    </Grid>
+                  )
+                }
+                return elements
+              })()}
+            </Grid>
           </Box>
         </Grid>
       </Grid>
-    </>
+    </Paper>
   )
 }
 
