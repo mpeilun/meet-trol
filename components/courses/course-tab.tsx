0da08c428d3f5dfadaab0a1b67aec45c660a5789
Peilun <peilun2016@gmail.com>
2023-02-13 22:23:24 +0800

    eyetracking

--- components/courses/course-tab.tsx
+++ components/courses/course-tab.tsx
@@ -8,6 +8,7 @@ import { CourseDataType } from '../../lib/dummy-data'
 import { display } from '@mui/system'
 import { Button, Divider, Icon } from '@mui/material'
 import { KeyboardArrowDown, OpenInNew } from '@mui/icons-material'
+import EyesTracking from '../eyetracking/eyetracking'
 
 interface TabPanelProps {
   children?: React.ReactNode
@@ -19,13 +20,7 @@ function TabPanel(props: TabPanelProps) {
   const { children, value, index, ...other } = props
 
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
+    <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
       {value === index && (
         <Box sx={{ p: 3 }}>
           <Typography component="span">{children}</Typography>
@@ -69,18 +64,11 @@ export default function CourseTabs(props: CourseTabsProps) {
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-        >
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
           <Tab label="總覽" {...a11yProps(0)} />
           <Tab label="教材" {...a11yProps(1)} />
-          <Tab
-            label="目錄"
-            {...a11yProps(2)}
-            sx={{ display: { md: 'none' } }}
-          />
+          <Tab label="學習紀錄" {...a11yProps(2)} />
+          <Tab label="目錄" {...a11yProps(3)} sx={{ display: { md: 'none' } }} />
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
@@ -97,6 +85,9 @@ export default function CourseTabs(props: CourseTabsProps) {
         <PDF></PDF>
       </TabPanel>
       <TabPanel value={value} index={2}>
+        <EyesTracking />
+      </TabPanel>
+      <TabPanel value={value} index={3}>
         <Box p={3} display="flex" justifyContent="space-between">
           <Typography sx={{ fontWeight: 'bold' }}>章節1</Typography>
           <Icon>
---
+++ components/eyetracking/eyetracking.tsx
@@ -0,0 +1,17 @@
+import webgazer from 'webgazer'
+
+function EyesTracking() {
+  webgazer
+    .setGazeListener(function (data, elapsedTime) {
+      if (data == null) {
+        return
+      }
+      var xprediction = data.x //these x coordinates are relative to the viewport
+      var yprediction = data.y //these y coordinates are relative to the viewport
+      console.log(elapsedTime) //elapsed time is based on time since begin was called
+    })
+    .begin()
+  return <></>
+}
+
+export default EyesTracking
