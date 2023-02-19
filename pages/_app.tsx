99b26ab4e7b91a089bf5a6fbd7d2a4f853fb52c6
mpeilun <peilun2016@gmail.com>
2023-02-20 01:07:28 +0800

    remove notification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,15 @@ import '../styles/globals.css'
 import '../styles/react-pdf-styles.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout/layout'
-import { NotificationContextProvider } from '../store/notification-context'
 import { Provider } from 'react-redux'
 import store from '../store/store'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <NotificationContextProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </NotificationContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </Provider>
   )
 }
