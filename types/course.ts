ce3f169b9ead6b1d05d07b54975c43498fb4402b
Peilun <peilun2016@gmail.com>
2023-03-23 21:11:11 +0800

    update

--- types/course.ts
+++ types/course.ts
@@ -1,7 +1,13 @@
 import { Course as PrismaCourse } from '@prisma/client'
-
+import { Chapter as PrismaChapter } from '@prisma/client'
+import { Video as PrismaVideo } from '@prisma/client'
 export interface CourseCreateType
-  extends Omit<
-    PrismaCourse,
-    'id' | 'create_At' | 'update_At' | 'membersId' | 'ownerId'
-  > {}
+  extends Omit<PrismaCourse, 'id' | 'create_At' | 'update_At' | 'membersId'> {}
+
+export interface CourseWithDetail extends PrismaCourse {
+  chapters: ChapterWithDetail[]
+}
+
+export interface ChapterWithDetail extends PrismaChapter {
+  videos: PrismaVideo[]
+}
