c54205aa30f3d54b36a80ad705f533927ebb16ed
Peilun <peilun2016@gmail.com>
2023-03-28 13:26:57 +0800

    update

--- pages/api/chapter/[id].ts
+++ pages/api/chapter/[id].ts
@@ -1,36 +1,88 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../../prisma/prisma'
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from '../auth/[...nextauth]'
+import { Course, Chapter } from '@prisma/client'
+import { CourseCreateType } from '../../../types/course'
+import { ChapterCreateType } from '../../../types/chapter'
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-  } else if (req.method === 'GET') {
-    const query = req.query as { id: string }
-    const isValidObjectId =
-      typeof query.id === 'string' &&
-      query.id.length === 24 &&
-      /^[a-f0-9]+$/i.test(query.id)
+  const session = await getServerSession(req, res, authOptions)
+  const { id } = req.query as { id: string }
 
-    if (isValidObjectId) {
-      const data = await prisma.chapter.findMany({
+  //判斷是否登入
+  if (session) {
+    if (req.method === 'POST') {
+      const data: ChapterCreateType = JSON.parse(req.body)
+      const create = await prisma.chapter.findUnique({
         where: {
-          courseId: query.id,
+          id: id,
+        },
+        
+      return res.status(201).json(create)
+    } else if (req.method === 'PUT') {
+      if (!courseId) {
+        return res
+          .status(404)
+          .json({ message: 'No Found, need parameter courseId: [objectId]' })
+      }
+      const data: Course = JSON.parse(req.body)
+      const check = await prisma.course.findUnique({
+        where: {
+          id: courseId,
         },
         select: {
-          title: true,
-          videos: {
-            select:{
-              id:true,
-              title: true
-            }
-          },
+          ownerId: true,
         },
       })
-      res.status(200).json(data)
+      if (check.ownerId.includes(session.user.id)) {
+        const update = await prisma.course.update({
+          where: {
+            id: courseId,
+          },
+          data: {
+            title: data.title,
+            description: data.description,
+            start: data.start,
+            end: data.end,
+          },
+        })
+
+        return res.status(201).json(update)
+      } else {
+        return res.status(403).json({ message: 'Forbidden' })
+      }
+    } else if (req.method === 'DELETE') {
+      if (!courseId) {
+        return res
+          .status(404)
+          .json({ message: 'No Found, need parameter courseId: [objectId]' })
+      }
+      const data: Course = req.body
+      const check = await prisma.course.findUnique({
+        where: {
+          id: courseId,
+        },
+        select: {
+          ownerId: true,
+        },
+      })
+      if (check.ownerId.includes(session.user.id)) {
+        const update = await prisma.course.delete({
+          where: {
+            id: courseId,
+          },
+        })
+
+        return res.status(204).json(update)
+      } else {
+        return res.status(403).json({ message: 'Forbidden' })
+      }
     } else {
-      res.status(400).json({ message: 'Error' })
+      return res.status(405).json({ message: 'Method Not Allowed' })
     }
   } else {
-    res.status(405).json({ message: 'Method not allowed' })
+    return res.status(401).json({ message: 'Unauthorized' })
   }
 }
 
