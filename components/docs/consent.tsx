08913318bad747e13d3c8ddd2471240f89e90241
David Lai <davidlai0263@gmail.com>
2023-04-11 16:12:37 +0800

    add obvious color hint

--- components/docs/consent.tsx
+++ components/docs/consent.tsx
@@ -2,10 +2,10 @@ import { Card, Typography } from "@mui/material"
 export const Consent = () => {
     return(
         <Card sx={{width: '100%', display:'flex', flexDirection:'column', p: 2, mb:2}}>
-            <Typography variant='h6' textAlign='center'>蒐集個人資料告知事項暨個人資料提供同意書</Typography>
+            <Typography variant='h6' textAlign='center'><strong>蒐集個人資料告知事項暨個人資料提供同意書</strong></Typography>
             <Typography variant='body2'>　因應個人資料保護法之規定，在向您蒐集個人資料之前，依法向您告知下列事項，當您簽署這份同意書，表示您已閱讀、瞭解並同意接受本同意書之所有內容：</Typography>
             <Typography variant='body2'><strong>•　一、蒐集目的及類別</strong></Typography>
-            <Typography variant='body2'>　為了順利進行實驗研究，我們需要蒐集參與者的個人資料，包括但不限於：<strong>學號</strong>、<strong>班級</strong>、<strong>姓名</strong>以及<strong>使用本實驗系統產生的所有電磁紀錄</strong>等資料。這些資料將用於研究分析、確認參與者身份以及內部管理等用途。</Typography>
+            <Typography variant='body2'>　為了順利進行實驗研究，我們需要蒐集參與者的個人資料，包括但不限於：<br/><strong  style={{color: 'red'}}>學號</strong>、<strong style={{color: 'red'}}>班級</strong>、<strong style={{color: 'red'}}>姓名</strong>以及<strong style={{color: 'red'}}>使用本實驗系統產生的所有電磁紀錄</strong>等資料。這些資料將用於研究分析、確認參與者身份以及內部管理等用途。</Typography>
             <Typography variant='body2'><strong>•　二、個人資料利用之期間、地區、對象及方式</strong></Typography>
             <Typography variant='body2'>　您的個人資料僅供本實驗單位於中華民國領域，於上述蒐集目的之必要合理範圍內加以利用至前述蒐集目的消失時為止。</Typography>
             <Typography variant='body2'><strong>•　三、當事人權利行使</strong></Typography>
